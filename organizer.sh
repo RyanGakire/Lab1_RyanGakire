7ee5a7408c38c9d9d3feb447db441486931baf60
RyanGakire <r.gakire@alustudent.com>
2026-07-22 12:41:41 +0200

    Generate timestamp for archived filename

--- organizer.sh
+++ organizer.sh
@@ -5,8 +5,12 @@ ARCHIVE_DIR="archive"
 LOG_FILE="organizer.log"
 SOURCE_FILE="grades.csv"
 
-# 1. Ensure the archive directory exists
+# Archive directory check and creation
 if [ ! -d "$ARCHIVE_DIR" ]; then
     mkdir "$ARCHIVE_DIR"
     echo "$(date): Created archive directory '$ARCHIVE_DIR'." >> "$LOG_FILE"
 fi
+
+# Timestamp generation
+TIMESTAMP=$(date +"%Y%m%d_%H%M%S")
+NEW_FILE_NAME="grades_$TIMESTAMP.csv"
